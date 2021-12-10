4a13730c7e82d5e0e55ce37d8b4d5ede7826027d
npa-dev <77854031+npa-dev@users.noreply.github.com>
2021-12-10 20:03:19 +0000

    Add files via upload

--- classes/post_class.php
+++ classes/post_class.php
@@ -1,1 +1,51 @@
+<?php
+//connect to database class
+require_once (dirname(__FILE__)) . '/../settings/db_class.php';
 
+class Post extends db_connection
+{
+    public function create($title, $body)
+    {
+        // sql query
+        $sql = "INSERT INTO `posts`(`title`, `body`) VALUES ('$title','$body')";
+
+        // run query
+        return $this->db_query($sql);
+    }
+
+    public function getPosts()
+    {
+        //sql query
+        $sql = "SELECT * FROM `posts`";
+
+        //run query
+        return $this->db_query($sql);
+    }
+
+    public function getSinglePost($id)
+    {
+        // sql query
+        $sql = "SELECT * FROM `posts` WHERE `id`='$id'";
+
+        // run query
+        return $this->db_query($sql);
+    }
+
+    public function update($id, $title, $body)
+    {
+        // sql query
+        $sql = "UPDATE `posts` SET `title`='$title',`body`='$body' WHERE `id`='$id'";
+
+        // run query
+        return $this->db_query($sql);
+    }
+
+    public function delete($id)
+    {
+        // sql query
+        $sql = "DELETE FROM `posts` WHERE `id`='$id'";
+
+        // run query
+        return $this->db_query($sql);
+    }
+}
