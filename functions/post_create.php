36349d0f51ffdae9f8643c2ef83fba41b634de37
npa-dev <77854031+npa-dev@users.noreply.github.com>
2021-12-10 20:07:11 +0000

    Add files via upload

--- functions/post_create.php
+++ functions/post_create.php
@@ -1,1 +1,16 @@
+<?php
+//connect to post controller
+include_once (dirname(__FILE__)) . '/../controllers/post_controller.php';
 
+
+if (isset($_POST['submit'])) {
+    // Grab form inputs
+    $title = $_POST['title'];
+    $body = $_POST['body'];
+
+    // create post if not empty
+    $newPost = createPost($title, $body);
+    if ($newPost) {
+        header("location: ../interactions.php");
+    }
+}
